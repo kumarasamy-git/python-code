94f7db0e03c43213e75ad32fc0735883163f3dd9
kumarasamy-git <37866159+kumarasamy-git@users.noreply.github.com>
2018-03-29 11:15:45 +0530

    Update Sample.java
    Added new print line

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Updated, Content1!");
+        System.out.println("Updated, Content2!");
     }
 }
